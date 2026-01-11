57a039657edd77dc8fd373bef842705689314448
NeunG01 <ponanan001@hotmail.com>
2026-01-11 11:35:15 +0700

    update profile template and settings

--- social/settings.py
+++ social/settings.py
@@ -68,8 +68,15 @@ WSGI_APPLICATION = 'social.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': os.getenv("DB_PASSWORD_YO"),
+        'HOST': 'yamanote.proxy.rlwy.net',
+        'PORT': '28404',
+        
     }
 }
 
